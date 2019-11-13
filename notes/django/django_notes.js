2a2f6563f5cf0b5cae7c95a8f35c0f2e3cdab368
jcom619 <46643961+jcom619@users.noreply.github.com>
2019-11-13 07:26:05 -0800

    major update still incomplete
    added js to menu button, only django notes and homepage working

---
+++ notes/django/django_notes.js
@@ -0,0 +1,38 @@
+function commands(){
+   var pic1 = "image/commands.png"
+   document.getElementById('thumb').src = pic1.replace('90x90', '225x225');
+
+}
+
+function fileStructure(){
+   var pic = "image/django_structure.png"
+   document.getElementById('thumb').src = pic.replace('90x90', '225x225');
+
+}
+
+function firstApp(){
+   var pic = "image/first_app1.png"
+   document.getElementById('thumb').src = pic.replace('90x90', '225x225');
+
+}
+
+function temps() {
+   var pic = 'image/templates.png'
+   document.getElementById('thumb').src = pic.replace('90x90', '225x225');
+}
+
+function static() {
+   var pic = 'image/static.png'
+   document.getElementById('thumb').src = pic.replace('90x90', '225x225');
+}
+
+function toolbar() {
+   var pic = 'image/toolbar.png'
+   document.getElementById('thumb').src = pic.replace('90x90', '225x225');
+}
+
+//  BACK BUTTON
+yes_back_button = function() {
+   // Your code here
+   return false;
+}
