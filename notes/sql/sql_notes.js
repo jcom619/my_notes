8ce136073cc34cfd3327493502b3b0433ba34d77
jcom619 <46643961+jcom619@users.noreply.github.com>
2019-11-13 15:17:29 -0800

    added sql note page

---
+++ notes/sql/sql_notes.js
@@ -0,0 +1,13 @@
+
+
+function setup() {
+   var pic = 'image/setup.png'
+   document.getElementById('thumb').src = pic.replace('90x90', '225x225');
+}
+
+
+//  BACK BUTTON
+yes_back_button = function() {
+   // Your code here
+   return false;
+}
