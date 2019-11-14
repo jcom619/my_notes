a30011a9523e03dea5721d749026c648375a5ceb
jcom619 <46643961+jcom619@users.noreply.github.com>
2019-11-13 17:49:51 -0800

    finished sql notes

--- notes/sql/sql_notes.js
+++ notes/sql/sql_notes.js
@@ -5,6 +5,26 @@ function setup() {
    document.getElementById('thumb').src = pic.replace('90x90', '225x225');
 }
 
+function create_table() {
+   var pic = 'image/create_table.png'
+   document.getElementById('thumb').src = pic.replace('90x90', '225x225');
+}
+
+function columns() {
+   var pic = 'image/columns.png'
+   document.getElementById('thumb').src = pic.replace('90x90', '225x225');
+}
+
+function values() {
+   var pic = 'image/values.png'
+   document.getElementById('thumb').src = pic.replace('90x90', '225x225');
+}
+
+function functions() {
+   var pic = 'image/functions.png'
+   document.getElementById('thumb').src = pic.replace('90x90', '225x225');
+}
+
 
 //  BACK BUTTON
 yes_back_button = function() {
