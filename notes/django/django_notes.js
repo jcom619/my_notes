93ee8e7b24e51ab250871e1abc110b42f28c8da7
jcom619 <46643961+jcom619@users.noreply.github.com>
2019-11-13 22:39:09 -0800

    Update django_notes.js

--- notes/django/django_notes.js
+++ notes/django/django_notes.js
@@ -21,15 +21,9 @@ function show(menuItem) {
       pic = "image/toolbar.png"
       break;
     default:
-
   }
   document.getElementById('thumb').src = pic.replace('90x90', '225x225');
 }
-
-
-
-
-
 
 //  BACK BUTTON
 yes_back_button = function() {
