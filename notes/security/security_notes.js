904babc9bb57387938c56c427505e05031c36b28
jcom619 <46643961+jcom619@users.noreply.github.com>
2019-11-14 10:33:55 -0800

    seecurity note update

--- notes/security/security_notes.js
+++ notes/security/security_notes.js
@@ -1,34 +1,13 @@
-function commands(){
-   var pic1 = "image/commands.png"
-   document.getElementById('thumb').src = pic1.replace('90x90', '225x225');
 
-}
-
-function fileStructure(){
-   var pic = "image/django_structure.png"
-   document.getElementById('thumb').src = pic.replace('90x90', '225x225');
-
-}
-
-function firstApp(){
-   var pic = "image/first_app1.png"
-   document.getElementById('thumb').src = pic.replace('90x90', '225x225');
-
-}
-
-function temps() {
-   var pic = 'image/templates.png'
-   document.getElementById('thumb').src = pic.replace('90x90', '225x225');
-}
-
-function static() {
-   var pic = 'image/static.png'
-   document.getElementById('thumb').src = pic.replace('90x90', '225x225');
-}
-
-function toolbar() {
-   var pic = 'image/toolbar.png'
-   document.getElementById('thumb').src = pic.replace('90x90', '225x225');
+function show(menuItem) {
+  var pic = "";
+  switch (menuItem) {
+    case 'injection':
+      pic = "../../images/argyle.png"
+      break;
+    default:
+  }
+  document.getElementById('thumb').src = pic.replace('90x90', '225x225');
 }
 
 //  BACK BUTTON
