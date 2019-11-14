1202e5f0958217e48874f579a486c0f2656afc5b
jcom619 <46643961+jcom619@users.noreply.github.com>
2019-11-13 19:30:40 -0800

    trick notes updated

---
+++ notes/tricks/tricks_notes.js
@@ -0,0 +1,33 @@
+//
+//
+// function setup() {
+//    var pic = 'image/setup.png'
+//    document.getElementById('thumb').src = pic.replace('90x90', '225x225');
+// }
+//
+// function create_table() {
+//    var pic = 'image/create_table.png'
+//    document.getElementById('thumb').src = pic.replace('90x90', '225x225');
+// }
+//
+// function columns() {
+//    var pic = 'image/columns.png'
+//    document.getElementById('thumb').src = pic.replace('90x90', '225x225');
+// }
+//
+// function values() {
+//    var pic = 'image/values.png'
+//    document.getElementById('thumb').src = pic.replace('90x90', '225x225');
+// }
+
+function Scroll() {
+   var pic = 'image/scroll.png'
+   document.getElementById('thumb').src = pic.replace('90x90', '225x225');
+}
+
+
+//  BACK BUTTON
+yes_back_button = function() {
+   // Your code here
+   return false;
+}
