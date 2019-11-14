bda24da374903cc36092a26c88130b6c7a91959e
jcom619 <46643961+jcom619@users.noreply.github.com>
2019-11-13 21:27:11 -0800

    update dropdown

--- notes/tricks/tricks_notes.js
+++ notes/tricks/tricks_notes.js
@@ -1,24 +1,4 @@
-//
-//
-// function setup() {
-//    var pic = 'image/setup.png'
-//    document.getElementById('thumb').src = pic.replace('90x90', '225x225');
-// }
-//
-// function create_table() {
-//    var pic = 'image/create_table.png'
-//    document.getElementById('thumb').src = pic.replace('90x90', '225x225');
-// }
-//
-// function columns() {
-//    var pic = 'image/columns.png'
-//    document.getElementById('thumb').src = pic.replace('90x90', '225x225');
-// }
-//
-// function values() {
-//    var pic = 'image/values.png'
-//    document.getElementById('thumb').src = pic.replace('90x90', '225x225');
-// }
+
 
 function Scroll() {
    var pic = 'image/scroll.png'
